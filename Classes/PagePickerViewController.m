d11a9a251c2fe2c047d879a656bc16cd54ad6e00
James Randall <james@accidentalfish.com>
2010-10-21 20:45:24 +0100

    Implemented page deletion, still needs a little work

--- Classes/PagePickerViewController.m
+++ Classes/PagePickerViewController.m
@@ -52,13 +52,11 @@
     // Release any cached data, images, etc that aren't in use.
 }
 
-
 - (void)viewDidUnload {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
-
 
 - (void)dealloc {
 	self.titles = nil;
